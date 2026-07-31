6390efc822d8f2ee6cbe2eed267c384f934e6922
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-07-31 23:19:11 +0530

    small change

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,8 +4,10 @@ export default function Homepage(props) {
     const [greetname , setgreetname]= useState("User")
 
   return (<>
+  <div className='homeposition'>
     <div><h3>{props.homepage}</h3></div>
-    <div className='usergreetings'> Good Morning </div>
+    <div className='usergreetings'> Good Morning Abu ,</div>
+    </div>
     </>
   )
 }
