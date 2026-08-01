e1c9718258250a52153aaccee48c8b55de45fa85
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-08-01 22:26:12 +0530

    localstorage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 
 export default function Homepage(props) {
-    const [greetname , setgreetname]= useState("User")
+    const [currentbalance , setcurrentbalance]=useState("")
+
+
+  
+ function editcb(){
+
+
+}
+
+ function savecb(){
+
+let a =localStorage.setItem("amount","currentbalance")
+
+}
+
+
 
   return (<>
   <div className='homeposition'>
     <div><h3>{props.homepage}</h3></div>
     <div className='usergreetings'> Good Morning Abu ,</div>
     </div>
+
+    {/* main box of CURRENT BALANCE  */}
     <div className='input-cb'>
       <p>CURRENT BALANCE </p>
     <div className="d-flex gap-2">
@@ -15,11 +32,22 @@ export default function Homepage(props) {
     type="text"
     className="form-control"
     placeholder="Enter Amount$"
-  />
-  <button className="btn btn-primary">Add</button>
-  <button className="btn btn-danger">Save</button>
+    value={currentbalance}
+    onChange={(e)=>setcurrentbalance(e.target.value)}
+    />
+    {localStorage.setItem("amount",currentbalance)}
+
+  <button className="btn btn-primary" onClick={editcb}>Edit</button>
+  <button className="btn btn-danger" onClick={savecb}>Save</button>
 </div>
+</div>
+
+<div className="container1">
+  <div className="box">Income </div>
+  <div className="box">Expense </div>
 </div>
     </>
   )
 }
+
+
