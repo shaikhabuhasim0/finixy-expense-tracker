d50aed57acd7990989db75ecae515e0f0995f83d
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-08-01 20:13:12 +0530

    Home page updates

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -8,6 +8,18 @@ export default function Homepage(props) {
     <div><h3>{props.homepage}</h3></div>
     <div className='usergreetings'> Good Morning Abu ,</div>
     </div>
+    <div className='input-cb'>
+      <p>CURRENT BALANCE </p>
+    <div className="d-flex gap-2">
+  <input
+    type="text"
+    className="form-control"
+    placeholder="Enter Amount$"
+  />
+  <button className="btn btn-primary">Add</button>
+  <button className="btn btn-danger">Save</button>
+</div>
+</div>
     </>
   )
 }
