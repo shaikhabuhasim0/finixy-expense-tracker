09f5cf14a8262e2f36af743a8527785bb74d9924
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-08-15 22:39:24 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
 import Homepage from './components/Homepage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
  function App() {
   return (
     <>
+    <BrowserRouter>
+<Routes>
+
+</Routes>
+    </BrowserRouter>
     <Navbar/>
     <Homepage homepage="HOME"/>
+    
     </>
   )
 }
