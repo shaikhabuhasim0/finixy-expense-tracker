d827de8021fed82938051ec905fe17777c408e26
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-08-19 22:54:15 +0530

    loan secntion

--- src/Components/Loan.jsx
+++ src/Components/Loan.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Loan() {
   return (
-    <div>Loan</div>
+    <div>Loan
+      thsai ia a loan sena ti fgs A
+      SFafjhak fakjsgk
+      alfhakjsh
+    </div>
   )
 }
