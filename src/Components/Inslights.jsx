28979521ae0d45e7b8b6ef3791186e60ea9e1199
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-09-02 22:50:20 +0530

    added

--- src/Components/Inslights.jsx
+++ src/Components/Inslights.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Inslights() {
   return (
     <div> yha per har type ki inslisghts available hai !!! 
-      
+      hahahahahahahahahahahahahahahahahahahah
     </div>
   )
 }
