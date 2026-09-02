1397bc6da9ad208dbe5537a4ccc0e9464108539c
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-09-02 22:48:18 +0530

    inslights updeted

--- src/Components/Inslights.jsx
+++ src/Components/Inslights.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Inslights() {
   return (
-    <div>Hey user 
+    <div> yha per har type ki inslisghts available hai !!! 
+      
     </div>
   )
 }
