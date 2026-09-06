79eb94c123da3226f086c600e22a15e86174247d
ABU HASIM <shaikhabuhasim0@gmail.com>
2026-09-06 22:51:31 +0530

    ADDED CARDS TO LOAN SECTION

--- src/Components/Loan.jsx
+++ src/Components/Loan.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Loan() {
   return (
+    <>
     <div>
       <div className="tran"><h3>LOANS</h3></div>
     </div>
+
+<div className='loancards'>
+  <div className='loancards1'>Total Loan</div>
+  <div className='loancards2'>Total Paid</div>
+</div>
+
+<div className='loancardsnosecond'>
+  <div className='loancardsno1'>Remaining</div>
+  <div className='loancardsno2'>Next Due</div>
+</div>
+
+    </>
   )
 }
